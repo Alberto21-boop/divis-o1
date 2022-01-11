ceea2ac4b66ec37a85f5ee7111e3ee090da04d96
Alberto Barbosa 021 <85910024+Alberto21-boop@users.noreply.github.com>
2022-01-11 13:52:10 -0300

    Add files via upload

---
+++ estudo1/estudo1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace estudo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a = 5.8;
+            double b = 4.8;
+
+            double resultado = a / b;
+
+            Console.WriteLine("O resultado desta divisão é " + resultado);
+        }
+    }
+}
